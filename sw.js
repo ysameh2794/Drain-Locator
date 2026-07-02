d236184cae7411e912a017f39c225e7fbba8659a
ysameh2794 <ysameh2794@gmail.com>
2026-07-02 03:22:42 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const cacheName = 'drain-app-v1';
+const assets = [
+  'index.html',
+  'manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.min.js',
+  'https://unpkg.com/@turf/turf@7/turf.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cachedResponse => cachedResponse || fetch(e.request)));
+});
